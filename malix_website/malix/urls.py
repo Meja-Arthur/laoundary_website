cdee2d694d96602baa691a24011fa01ac30f85c1
Arthur._.Kings <arthurogembo@gmail.com>
2023-08-26 09:13:17 +0300

    updating my git codes

--- malix_website/malix/urls.py
+++ malix_website/malix/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import my_view, about
+from .views import my_view, about, single, service, price, contact, blog
 app_name = 'malix'
 
 urlpatterns = [
     path('', my_view, name='index'),
-    path('about/', about, name='about-page')
+    path('about/', about, name='about-page'),
+    path('single/', single, name='single'),
+    path('service/', service, name='service'),
+    path('price/', price, name='pricing_page'),
+    path('contact/', contact, name='contact-page'),
+    path('blog/', blog, name='blog-page')
 ]
--- malix_website/malix/views.py
+++ malix_website/malix/views.py
@@ -7,3 +7,13 @@ def my_view(request):
     return render(request, 'index.html')
 def about(request):
     return render(request, 'about.html')
+def single(request):
+    return render(request,'single.html')
+def service(request):
+    return render(request,'service.html')
+def price(request):
+    return render(request,'pricing.html')
+def contact(request):
+    return render(request,'contact.html')
+def blog(request):
+    return render(request,'blog.html')
